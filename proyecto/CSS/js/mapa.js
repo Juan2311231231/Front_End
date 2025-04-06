a460e3f1d276f84d62a26adcd989cf934ca0cec8
SantiagoPoligran <astorres@poligran.edu.co>
2025-04-06 16:09:43 -0500

    Add files via upload

---
+++ proyecto/CSS/js/mapa.js
@@ -0,0 +1,55 @@
+{
+    "mapa_conceptual": {
+      "idea_central": "El Hombre como Ser Social por Naturaleza",
+      "ramas": [
+        {
+          "nombre": "Concepto Fundamental",
+          "subramas": [
+            "Ser humano gregario",
+            "Necesidad de la sociedad",
+            "La unión hace la fuerza"
+          ]
+        },
+        {
+          "nombre": "Características de la Sociabilidad",
+          "subramas": [
+            "Asociación con fines",
+            "Formas de sociabilidad (familias, ciudades, etc.)",
+            "Necesidad y ayuda mutua"
+          ]
+        },
+        {
+          "nombre": "Origen de la Sociabilidad",
+          "subramas": [
+            "Don divino",
+            "Imagen de Dios (Trinidad)",
+            "Comunión deseada por Dios"
+          ]
+        },
+        {
+          "nombre": "Proceso de Socialización",
+          "subramas": [
+            "Integración por valores culturales",
+            "Rol de las instituciones",
+            "Maduración en la relación"
+          ]
+        },
+        {
+          "nombre": "Obstáculos a la Sociabilidad",
+          "subramas": [
+            "Soberbia y egoísmo (pecado)",
+            "Generación del mal",
+            "Redención por Jesucristo"
+          ]
+        },
+        {
+          "nombre": "Puntos Clave Adicionales",
+          "subramas": [
+            "Aislamiento vs. asociación",
+            "Naturaleza social en la historia",
+            "Referencias bíblicas"
+          ]
+        }
+      ]
+    }
+  }
